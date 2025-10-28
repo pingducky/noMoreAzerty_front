dd1b117692e9131289faa6c81da0da45eb8adc32
Faxiis <antho.lavoute@gmail.com>
2025-10-28 12:25:21 +0100

    Init de l'authentification via Microsoft Entra ID

--- noMoreAzerty_front/Program.cs
+++ noMoreAzerty_front/Program.cs
@@ -6,6 +6,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Configuration de l'authentification avec Azure AD
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://67ee7997-1621-46ac-ada5-c49204f57d56/API.Access"); // Remplacez <API_ClientId> par l'ID client de votre API
+});
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
