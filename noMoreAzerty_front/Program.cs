546bf767c8ae16955ce38773e0a142f017344a3a
Faxiis <antho.lavoute@gmail.com>
2025-10-29 09:20:04 +0100

    Fix auth via entra id

--- noMoreAzerty_front/Program.cs
+++ noMoreAzerty_front/Program.cs
@@ -1,18 +1,25 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using noMoreAzerty_front;
+using noMoreAzerty_front.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 // Configuration de l'authentification avec Azure AD
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://67ee7997-1621-46ac-ada5-c49204f57d56/API.Access"); // Remplacez <API_ClientId> par l'ID client de votre API
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://67ee7997-1621-46ac-ada5-c49204f57d56/API.Access");
+    options.ProviderOptions.LoginMode = "redirect";
 });
 
+//builder.Services.AddScoped<UserService>();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 await builder.Build().RunAsync();
---
+++ noMoreAzerty_front/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+
+namespace noMoreAzerty_front.Services
+{
+    public class UserService
+    {
+        private readonly HttpClient _http;
+
+        public UserService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task InitUserAsync()
+        {
+            await _http.GetAsync("api/account/me");
+        }
+    }
+}
