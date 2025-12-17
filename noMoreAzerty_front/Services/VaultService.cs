822af3ccd3776aadaa5f22c732680cde5c8c3212
Faxiis <antho.lavoute@gmail.com>
2025-12-17 09:53:52 +0100

    Laision avec API pour lister les coffres et créer un vault

--- noMoreAzerty_front/Services/VaultService.cs
+++ noMoreAzerty_front/Services/VaultService.cs
@@ -22,6 +22,20 @@ public class VaultService
         return JsonSerializer.Deserialize<List<Vault>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
     }
 
+    public async Task<Vault> CreateVaultAsync(CreateVaultRequestDto createVaultRequest)
+    {
+        var jsonContent = new StringContent(JsonSerializer.Serialize(createVaultRequest), System.Text.Encoding.UTF8, "application/json");
+        // debug
+        Console.WriteLine("Creating vault with request: " + JsonSerializer.Serialize(createVaultRequest));
+        var response = await _httpClient.PostAsync("api/vault", jsonContent);
+        // debug
+        Console.WriteLine("Response status code: " + response.StatusCode);
+        Console.WriteLine("Response content: " + await response.Content.ReadAsStringAsync());
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<Vault>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true })!;
+    }
+
     public class Vault
     {
         public Guid Id { get; set; }
@@ -29,6 +43,21 @@ public class VaultService
         public string? HashPassword { get; set; }
         public string? PasswordSalt { get; set; }
         public DateTime CreatedAt { get; set; }
+    }
+
+    public class CreateVaultRequestDto // Todo : placer dans lib partagé /!\
+    {
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Mot de passe dérivé côté client (ex: via PBKDF2/Argon2)
+        /// </summary>
+        public string DerivedPassword { get; set; } = null!;
+
+        /// <summary>
+        /// Sel généré côté client
+        /// </summary>
+        public string PasswordSalt { get; set; } = null!;
     }
 
 }
